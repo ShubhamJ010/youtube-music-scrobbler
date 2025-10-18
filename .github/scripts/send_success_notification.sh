b5ca3e7b8be625b3f3ab9257031f39c0091ed5d2
Shubham Jha <samxn81@gmail.com>
2025-10-19 04:08:27 +0530

    script

---
+++ .github/scripts/send_success_notification.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+log_content="${SCROBBLE_LOG}"
+# Escape for JSON
+log_content="${log_content//\\/\\\\}"
+log_content="${log_content//"/\"}"
+log_content="${log_content//$'
+'/\n}"
+
+if [ ${#log_content} -gt 1980 ]; then
+  log_content="$(echo "$log_content" | cut -c 1-1980)... (log truncated)"
+fi
+
+json_payload=$(printf '{"content": "YouTube Music Scrobble Sync succeeded!", "embeds": [{"title": "Scrobble Log", "description": "```\n%s\n```"}]}' "$log_content")
+
+curl -H "Content-Type: application/json" -d "$json_payload" "$DISCORD_WEBHOOK_URL"
