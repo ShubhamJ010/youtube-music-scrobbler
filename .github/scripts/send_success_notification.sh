9bdf52c79c8af7be894f1b08ef14e926099e6035
Shubham Jha <samxn81@gmail.com>
2025-10-19 04:12:42 +0530

    Troubleshoot: Simplify notification script

--- .github/scripts/send_success_notification.sh
+++ .github/scripts/send_success_notification.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
-
 set -e
-
-log_content="${SCROBBLE_LOG}"
-# Escape for JSON
-log_content="${log_content//\\/\\\\}"
-log_content="${log_content//"/\"}"
-
-# Replace newlines with \n
-log_content_escaped_newlines=""
-while IFS= read -r line; do
-  log_content_escaped_newlines+="$line\n"
-done <<< "$log_content"
-log_content="$log_content_escaped_newlines"
-
-
-if [ ${#log_content} -gt 1980 ]; then
-  log_content="$(echo "$log_content" | cut -c 1-1980)... (log truncated)"
-fi
-
-json_payload=$(printf '{"content": "YouTube Music Scrobble Sync succeeded!", "embeds": [{"title": "Scrobble Log", "description": "```\n%s\n```"}]}' "$log_content")
-
-curl -H "Content-Type: application/json" -d "$json_payload" "$DISCORD_WEBHOOK_URL"
+echo "Script starts"
+echo "Log content is: ${SCROBBLE_LOG}"
+echo "Script ends"
